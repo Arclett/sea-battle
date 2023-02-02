1bb6f1aee64ef322261580d9917d4ff6d27562d8
Aleksei Stoyanenko <stoynalex@gmail.com>
2023-02-02 20:23:27 +0500

    feat: add render methods for rendering login window

--- src/scripts/classes/rendering/_renderLoginWindow.ts
+++ src/scripts/classes/rendering/_renderLoginWindow.ts
@@ -1,3 +1,18 @@
 export class RenderLoginWindow {
-    static render(container: HTMLElement) {}
+    static render(container: HTMLElement) {
+        const overlay = document.createElement("div");
+        overlay.className = "overlay";
+
+        const loginWrapper = document.createElement("div");
+        loginWrapper.className = "overlay";
+
+        const loginTitle = document.createElement("h2");
+        loginTitle.textContent = "Log or Sign in";
+
+        const loginButton = document.createElement("button");
+        loginButton.textContent = "Login";
+
+        const signButton = document.createElement("button");
+        signButton.textContent = "Sign";
+    }
 }
