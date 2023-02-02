ddbad63d8ff85dba1f0ea376646295fbf3d92a9e
balaxon <balahanov2209@gmail.com>
2023-02-02 20:20:10 +0300

    feat: add interfaces and method to create elements

---
+++ src/scripts/classes/element/element.ts
@@ -0,0 +1,46 @@
+import { ElementInterface, InputElement } from "../../interfaces";
+
+
+
+export class Element {
+  createElement({ tag, id, classNote, content, disable = false }: ElementInterface) {
+    const el = document.createElement(tag);
+    if (id) {
+      el.id = id;
+    }
+    if (classNote) {
+      el.classList.add(classNote);
+    }
+    if (content) {
+      el.textContent = content;
+    }
+    if (disable) {
+      (el as HTMLButtonElement).disabled = disable;
+    }
+    return el;
+  }
+
+  createInput({ type, id, classNote, value, content, name, disable = false }: InputElement) {
+    const el = document.createElement('input');
+    el.type = type;
+    el.disabled = disable;
+    if (id) {
+      el.id = id;
+    }
+    if (classNote) {
+      el.classList.add(classNote);
+    }
+    if (value) {
+      el.value = value;
+    }
+    if (content) {
+      el.textContent = content;
+    }
+    if (name) {
+      el.name = name;
+    }
+    return el;
+  }
+
+  
+}
