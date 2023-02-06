add9c5ff7ca7b778a45aa41ba26fd6be8ff3dcd8
balaxon <balahanov2209@gmail.com>
2023-02-06 11:43:41 +0300

    feat: add basic router

--- src/scripts/classes/Main.ts
+++ src/scripts/classes/Main.ts
@@ -15,9 +15,15 @@ export class Main {
         this.loginWindow = new LoginWindow();
         document.body.addEventListener("click", this.clickHandler.bind(this));
         window.addEventListener("beforeunload", this.socketHandler.saveToLocalStorage.bind(this));
+        this.appRouting(location.hash);
+        window.addEventListener('hashchange', () => {
+            //если хеш изменился то вызываем роутинг
+            this.appRouting(location.hash);
+        }, false);
     }
 
     clickHandler(e: Event) {
+        //изменяем хеш при клике
         e.preventDefault();
         if (!(e.target instanceof HTMLElement)) return;
 
@@ -29,8 +35,24 @@ export class Main {
         if (e.target.classList.contains("login__overlay")) this.loginWindow.closeWindow();
         if (e.target.classList.contains("login__enter-button")) this.authorization(e.target);
         if (e.target.classList.contains("login__regist-button")) this.loginWindow.switchLoginWindowMode(e.target);
-
         //PlayFiled events
+    }
+
+    appRouting(hash: string) {
+        // для примера, поменять в адресной строке хеш на page и любой другой
+        switch (hash) {
+            case '#page':
+                //this.page();
+                console.log('page');
+                break;
+            case '#?game=':
+
+                break;
+        
+            default:
+                console.log('hash changed')
+                break;
+        }
     }
 
     authorization(elem: HTMLElement) {
